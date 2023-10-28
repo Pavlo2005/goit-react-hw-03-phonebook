b78bca72f5177719468d47b14e63cfecf86e8a26
Pavlo2005 <savyukpavlo8@gmail.com>
2023-10-28 15:51:33 +0200

    commit done

--- src/components/ContactsCard/ContactsCard.styled.js
+++ src/components/ContactsCard/ContactsCard.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.li`
+export const Container = styled.div`
     display: flex;
     align-items: center;
     gap: ${p => p.theme.spacing(4)};
